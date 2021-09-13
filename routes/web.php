49606f0830aa76bfa505973b3db9d3fb97e141dc
vinson <1535372340@qq.com>
2021-09-13 22:50:27 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,6 @@ Route::resource('users','UsersController',['only' => ['show','update','edit']]);
  *  Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
  *  Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
  */
+
+// 话题相关
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
