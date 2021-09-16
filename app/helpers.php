6ac51b8e44ed4a99f5833cf45b382799df2c5290
vinson <1535372340@qq.com>
2021-09-17 00:17:38 +0800

    用户可以创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -15,3 +15,15 @@ function category_nav_active($category_id)
 {
     return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
 }
+
+/**
+ * 过滤并截取文章内容（用于话题摘录 作为文章页面的 description 元标签使用）
+ * @param $value
+ * @param int $length
+ * @return mixed
+ */
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return Str::limit($excerpt, $length);
+}
